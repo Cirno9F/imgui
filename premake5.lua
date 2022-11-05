22414b9ebbaa61773c4ba3b187249dc0404a996e
Cirno9F <2380604115@qq.com>
2022-11-05 16:40:35 +0800

    Change premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,21 +22,8 @@ project "ImGui"
 		"imstb_truetype.h",
 	}
 
-    defines
-    {
-        "IMGUI_API=_declspec(dllexport)"
-    }
-
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
